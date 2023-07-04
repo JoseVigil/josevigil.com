b6907770fc3eaadb987552344f6395d4ee93d1d2
Kevin <kevinfiorentino.1294@gmail.com>
2023-07-04 12:11:36 -0300

    Change schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,10 +3,7 @@ import { defineCollection, z } from 'astro:content';
 const mainSchema = z.object({
   title: z.string(),
   description: z.string(),
-  image: z.object({
-    url: z.string(),
-    alt: z.string()
-  }),
+  image: z.string().url(),
   pubDate: z.string().transform((str) => new Date(str)),
   tags: z.array(z.string()),
 });

29f48d174d680dc1b5c516abfca54506e6d80206
Kevin <kevinfiorentino.1294@gmail.com>
2023-07-03 22:40:56 -0300

    Re-estruct content and styles

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,5 @@
 import { defineCollection, z } from 'astro:content';
 
-
-/* ******************************
-         ASTRO CONFIGS
-****************************** */
-
-const relatedSchema = z.object({
-  collection: z.string(),
-  slug: z.string(),
-});
-
 const mainSchema = z.object({
   title: z.string(),
   description: z.string(),
@@ -19,15 +9,10 @@ const mainSchema = z.object({
   }),
   pubDate: z.string().transform((str) => new Date(str)),
   tags: z.array(z.string()),
-  relatedPosts: z.array(relatedSchema).optional().nullable(),
 });
 
-const maularCollection = defineCollection({ schema: mainSchema });
-const nenesCollection = defineCollection({ schema: mainSchema });
-const uitoolsCollection = defineCollection({ schema: mainSchema });
+const essaysCollection = defineCollection({ schema: mainSchema });
 
 export const collections = {
-  'maular': maularCollection,
-  'nenes': nenesCollection,
-  'uitools': uitoolsCollection,
+  'essays': essaysCollection,
 };

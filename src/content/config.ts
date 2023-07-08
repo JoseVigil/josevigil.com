771dca4120f1e587620c6d8b44b735b8757ef98e
Kevin <kevinfiorentino.1294@gmail.com>
2023-07-08 15:28:58 -0300

    Update Astro version

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,7 +3,7 @@ import { defineCollection, z } from 'astro:content';
 const mainSchema = z.object({
   title: z.string(),
   description: z.string(),
-  image: z.string().url(),
+  image: z.string(), // mm/dd/yyyy
   pubDate: z.string().transform((str) => new Date(str)),
   tags: z.array(z.string()),
 });

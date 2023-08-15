9107a2ffa6c3bc0ef25bfb2e27c7ab44bce14d31
Jose Vigil <jose.vigil@roamtouch.com>
2023-08-15 02:24:54 -0300

    Added Essays, Project and Previous.

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,15 +4,17 @@ const mainSchema = z.object({
   title: z.string(),
   description: z.string(),
   image: z.string(), // mm/dd/yyyy
+  link: z.string().optional().nullable(),
   pubDate: z.string().transform((str) => new Date(str)),
   tags: z.array(z.string()),
 });
 
 const essaysCollection = defineCollection({ schema: mainSchema });
-
 const projectCollection = defineCollection({ schema: mainSchema });
+const previousCollection = defineCollection({ schema: mainSchema });
 
 export const collections = {
   'essays': essaysCollection,
   'projects': projectCollection,
+  'previous': previousCollection,
 };
